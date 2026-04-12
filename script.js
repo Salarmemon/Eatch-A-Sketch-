eba76bcc7ebe48a9844b343bd750a5e82628fba8
Salar Memon <142799802+Salarmemon@users.noreply.github.com>
2026-04-12 18:03:23 +0500

    Add styling and functionallity for grid.
    now on page load grid will be dynamically filled with set number of divs.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const getElm = (value, method, isAll=false) => {
      "Method not supported";
 }
 
-const generateDiv = (divId) => {
+const generateDiv = (divId, width, height) => {
     const div = document.createElement("div");
     div.classList.add("drawing-div");
     div.id = divId;
@@ -22,10 +22,20 @@ const modeElms = getElm("mode", "class", true);
 const clearBtn = getElm("clear-btn", "id");
 const drawingGrid = getElm("drawing-grid", "id");
 const initialGridSize = 16;
+const gridSlidbar = getElm("grid-slidbar", "id");
+const gridValueElm = getElm("grid-value", "id");
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    for (let i = 0; i <= initialGridSize; i++) {
+    const totalDivs = initialGridSize * initialGridSize;
+    drawingGrid.style.display = "grid";
+    drawingGrid.style.gridTemplateColumns = `repeat(${initialGridSize}, 1fr)`;
+
+
+    for (let i = 0; i < totalDivs; i++) {
         const gridDiv = generateDiv(`div-${i}`);
+
         drawingGrid.appendChild(gridDiv);
     }
 })
