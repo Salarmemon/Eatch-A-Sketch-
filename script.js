230edaf51cc079ecdf5daa907bde7880449d1b23
Salar Memon <142799802+Salarmemon@users.noreply.github.com>
2026-04-13 18:03:10 +0500

    Add proper functionality to draw.
    Added functions to draw user selected color as well as gave user the ability to select rainboow mode.
    Added a function to generate random colors that is very helpful while user selects rainbow mode.

--- script.js
+++ script.js
@@ -9,13 +9,45 @@ const getElm = (value, method, isAll=false) => {
      "Method not supported";
 }
 
+//function to generate a random color
+
+const generateRandomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+
+}
+
+
+//for simplicity generate div in seperate function
 const generateDiv = (divId, width, height) => {
     const div = document.createElement("div");
     div.classList.add("drawing-div");
     div.id = divId;
+    div.addEventListener("mousemove", (e) => {
+        
+        let currentOpacity = parseFloat(e.target.style.opacity) || 0;
+        currentOpacity += 0.1;
+        if (currentOpacity > 1) currentOpacity = 1;
+
+        e.target.style.opacity = currentOpacity;
+        //change color according to selected mode;
+        if (mode === "color-mode") {
+        e.target.style.backgroundColor = divColor;
+        } else if (mode === "rainbow-mode") {
+            e.target.style.backgroundColor= generateRandomColor();
+        } else if (mode === "black-and-white-mode") {
+            let color = Math.random() > 0.5 ? "black" : "white";
+            e.target.style.backgroundColor = color;
+        }
+    })
 
     return div;
 }
+
+//element references and variables
 
 const colorPickerElm = getElm("color-picker", "id");
 const modeElms = getElm("mode", "class", true);
@@ -24,6 +56,23 @@ const drawingGrid = getElm("drawing-grid", "id");
 const initialGridSize = 16;
 const gridSlidbar = getElm("grid-slidbar", "id");
 const gridValueElm = getElm("grid-value", "id");
+let divColor = "black ";
+let mode = "color-mode";
+
+
+//let the user select color
+colorPickerElm.addEventListener("change", (e) => {
+    divColor = e.target.value;
+})
+//let the user select drawing mode
+modeElms.forEach((elm) => {
+    elm.style.backgroundColor = "none";
+    elm.addEventListener("click", (e) => {
+        mode = e.target.id;
+        e.target.style.border = "3px solid green";
+    })
+})
+
 
 
 
