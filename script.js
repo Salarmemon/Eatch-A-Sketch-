c0f6ffde70906b2f2972c9fc36b8b6ae94a62020
Salar Memon <142799802+Salarmemon@users.noreply.github.com>
2026-04-16 17:47:42 +0500

    Fix slidebar and grid sizing bug

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const generateRandomColor = () => {
 
 
 //for simplicity generate div in seperate function
-const generateDiv = (divId, width, height) => {
+const generateDiv = (divId) => {
     const div = document.createElement("div");
     div.classList.add("drawing-div");
     div.id = divId;
@@ -55,9 +55,10 @@ const colorPickerElm = getElm("color-picker", "id");
 const modeElms = getElm("mode", "class", true);
 const clearBtn = getElm("clear-btn", "id");
 const drawingGrid = getElm("drawing-grid", "id");
-const initialGridSize = 16;
+let initialGridSize = 16;
 const gridSlidbar = getElm("grid-slidbar", "id");
 const gridValueElm = getElm("grid-value", "id");
+
 let divColor = "black ";
 let mode = "color-mode";
 
@@ -83,6 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const totalDivs = initialGridSize * initialGridSize;
     drawingGrid.style.display = "grid";
     drawingGrid.style.gridTemplateColumns = `repeat(${initialGridSize}, 1fr)`;
+    drawingGrid.style.gridTemplateRows = `repeat(${initialGridSize}, 1fr)`;
 
 
     for (let i = 0; i < totalDivs; i++) {
@@ -99,4 +101,19 @@ clearBtn.addEventListener("click", () => {
         div.style.backgroundColor = "white";
         div.style.opacity = "0";
     })
+})
+
+gridSlidbar.addEventListener("change", (e) => {
+    const gridSize = e.target.value;
+    const totalDivs = gridSize ** 2;
+    drawingGrid.innerHTML = "";
+     
+    drawingGrid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    drawingGrid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    gridValueElm.textContent = gridSize;
+    for (let i = 0; i < totalDivs; i++) {
+        const gridDiv = generateDiv(`div-${i}`);
+
+        drawingGrid.appendChild(gridDiv);
+    }
 })
